697d2586c82ee872f1819902f6331925ecfce1a6
SofiaER <sofiautneguiazu@gmail.com>
2023-08-27 11:40:50 -0300

    Comienzo modelado Blog
    comienzo y modelado base de la seccion blog

--- frontend/src/app/Components/blog/blog.component.ts
+++ frontend/src/app/Components/blog/blog.component.ts
@@ -6,20 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./blog.component.css']
 })
 
+
 export class BlogComponent {
-  articles = [
+  blogPosts = [
     {
-      id: 1,
-      title: 'Título del Artículo 1',
-      imageUrl: 'ruta-de-la-imagen-1.jpg',
-      excerpt: 'Breve descripción del artículo 1...',
+      title: 'El arte de entrenar',
+      subtitle: 'Las 5 reglas para un entrenamiento eficiente.',
+      date: '27/08/2022',
+      image: 'https://img.freepik.com/foto-gratis/hombre-joven-fitness-estudio_7502-5008.jpg'
     },
     {
-      id: 2,
-      title: 'Título del Artículo 2',
-      imageUrl: 'ruta-de-la-imagen-2.jpg',
-      excerpt: 'Breve descripción del artículo 2...',
+      title: 'Quemar calorías en reposo luego de hacer ejercicio',
+      subtitle: 'La magia del efecto EPOC',
+      date: '25/03/2023',
+      image: 'https://media.istockphoto.com/id/1145802290/es/vector/l%C3%ADnea-de-acabado-de-carrera-de-corredor-y-silueta-de-campo.jpg?s=612x612&w=0&k=20&c=qdmyCbmvdcQP5uPghTqeVuzWYe-H-KGPruD0m82MqI8='
     },
-    // Agrega más artículos aquí
+    {
+      title: 'Beneficios de una Alimentación Saludable en tu Rutina de Ejercicio',
+      subtitle: 'Cómo una dieta equilibrada potencia tus resultados en el gimnasio',
+      date: '05/07/2023',
+      image: 'https://s03.s3c.es/imag/_v0/770x420/4/3/0/alimentos-deportistas-770.jpg'
+    }
   ];
 }
