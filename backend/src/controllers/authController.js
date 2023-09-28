a94bb9e9a1e49c615ad4a4b627a1f8a01e143ddf
= <=>
2023-09-28 10:30:51 -0300

    Se inicio pagos

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -83,25 +83,5 @@ const Signup = async (req, res) => {
 
 
 
-async function VerificoToken(res, req, next) {
-  try {
-    if (!req.headers.authorization) {
-      return res.status(401).send("no autorizado");
-    }
-    let token = req.headers.authorization.split(" ")[1];
-    if (token === "null") {
-      return res.status(401).send("Unauhtorized Request");
-    }
 
-    const payload = await jwt.verify(token, "secretkey");
-    if (!payload) {
-      return res.status(401).send("Unauhtorized Request");
-    }
-    req.userId = payload._id;
-    next();
-  } catch (e) {
-    //console.log(e)
-    return res.status(401).send("Unauhtorized Request");
-  }
-}
 module.exports = { LogInUser, Signup, VerificoToken };
--- frontend/src/app/Services/user.service.ts
+++ frontend/src/app/Services/user.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  ProximoPago(token:string): Observable<any>
+  {
+    return this.http.post<any>("http://localhost:3000/api/ProximoPago",token)
+  }
+
 }
