4df33c0976fd408ba67a72600921b64ca492451e
= <=>
2023-11-10 18:54:03 -0300

    Guard Para rutas  admin

---
+++ frontend/src/app/admin.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { LoginService } from './Services/login.service';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+  constructor(private _authService:LoginService,private _router:Router ,private cookies:CookieService){}
+
+  canActivate():boolean{
+    if(this._authService.LoggedIn() && this.cookies.get('rol')==='0' )
+    {
+      return true
+    }
+    else
+    {
+      this._router.navigate(['/home'])
+      return false
+    }
+  }
+  
+}
