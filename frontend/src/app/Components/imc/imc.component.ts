50d5c710195779edc85b80c848107d1f186e357e
= <=>
2023-12-07 18:31:15 -0300

    Error de relaciones en la db

--- frontend/src/app/Components/imc/imc.component.ts
+++ frontend/src/app/Components/imc/imc.component.ts
@@ -1,73 +1,64 @@
 import { Component } from '@angular/core';
+import { Alerts } from 'Alerts';
 import { ImcService } from 'src/app/Services/imc.service';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-imc',
   templateUrl: './imc.component.html',
-  styleUrls: ['./imc.component.css']
+  styleUrls: ['./imc.component.css'],
 })
 export class IMCComponent {
-IMC=
-{
-    altura:'',
-    peso:'',
-    edad: '', 
-    
-}
-imc:any
-bmr:any
-bfp:any
-sexo:string='';
+  IMC = {
+    altura: '',
+    peso: '',
+    edad: '',
+  };
+  imc: any;
+  bmr: any;
+  bfp: any;
+  sexo: string = '';
 
-  constructor(private imcservice: ImcService) {  this.imcservice.GetSexo(localStorage.getItem('token')!)
-  .subscribe((data:any)=>{data='femenino'?this.sexo='female':this.sexo='male'},
-  (error:any)=>{console.log(error.message)}) }
-   
+  constructor(private imcservice: ImcService) {
+    this.imcservice.GetSexo(localStorage.getItem('token')!).subscribe(
+      (data: any) => {
+        data = 'femenino' ? (this.sexo = 'female') : (this.sexo = 'male');
+      },
+      (error: any) => {
+        console.log(error.message);
+      }
+    );
+  }
 
-  async CalcularIMC()
-  { 
-     this.imcservice.cobinarLlmadosApi(Number.parseInt(this.IMC.altura),Number.parseInt( this.IMC.peso), Number.parseInt(this.IMC.edad), this.sexo)
-     .subscribe(
-      (data:any)=>{
-      Swal.fire({
-        icon: 'info',
-        title: 'Informe de sus salud',
-        html:` Indice de masa Corporal :<strong> ${data[0].info.bmi} </strong> </br>
+  async CalcularIMC() {
+    this.imcservice
+      .cobinarLlmadosApi(
+        Number.parseInt(this.IMC.altura),
+        Number.parseInt(this.IMC.peso),
+        Number.parseInt(this.IMC.edad),
+        this.sexo
+      )
+      .subscribe({
+        next: (data: any) => {
+          Swal.fire({
+            icon: 'info',
+            title: 'Informe de sus salud',
+            html: ` Indice de masa Corporal :<strong> ${data[0].info.bmi} </strong> </br>
                su salud segun estos datos:<strong> ${data[0].info.health}</strong> 
                <p>su metabolismo basal es de: <strong>${data[1].info.bmr} </strong> </p>
                <p>su porcentaje de grasa corporal es de: <strong>${data[3].info.bfp} </strong> </p>
                <p>su peso ideal es de: <strong>${data[2].info.devine}(Indide de devine) </strong> </p>
                `,
-        footer: '<a href="#">Para asesoria de nuestros profecionales</a>'
-        
-      })
-  
-      
-      
-      
-      },
-     (error:any)=>{console.log(error.message)
-    
-    
-      Swal.fire(
-        'Error en el servidor',
-        'si persiste comunicare al 0800-0800-0800-0800',
-        'error'
-      )
-    
-    
-    
-    
-    
-    })
-    
-  
-      
-    
+            footer: '<a href="#">Para asesoria de nuestros profecionales</a>',
+          });
+        },
+        error: (error: any) => {
+          Alerts(
+            'error',
+            'Error en el servidor',
+            'si persiste comunicare al 0800-0800-0800-0800'
+          );
+        },
+      });
   }
-
 }
-
-
-
